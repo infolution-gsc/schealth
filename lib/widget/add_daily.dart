564920f675bf3b8d2fd3eae439a0c78dbb438860
Ahmad Fauzan <ahmadfauzan190603@gmail.com>
2023-02-28 14:12:13 +0700

    add_daily

--- lib/widget/add_daily.dart
+++ lib/widget/add_daily.dart
@@ -3,13 +3,68 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:healthy_planner/utils/theme.dart';
 
 class AddDaily extends StatefulWidget {
-  const AddDaily({super.key});
+  const AddDaily({super.key, this.restorationId});
 
   @override
   State<AddDaily> createState() => _AddDailyState();
+
+  final String? restorationId;
 }
 
-class _AddDailyState extends State<AddDaily> {
+class _AddDailyState extends State<AddDaily> with RestorationMixin {
+  @override
+  String? get restorationId => widget.restorationId;
+
+  final RestorableDateTime _selectedDate =
+      RestorableDateTime(DateTime(2023, 2, 27));
+  late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
+      RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+
+  static Route<DateTime> _datePickerRoute(
+    BuildContext context,
+    Object? arguments,
+  ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendarOnly,
+          initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
+          firstDate: DateTime(2022),
+          lastDate: DateTime(2025),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(
+        _restorableDatePickerRouteFuture, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+              'Selected: ${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+        ));
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -137,9 +192,38 @@ class _AddDailyState extends State<AddDaily> {
                     ),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 5,
                 ),
+                Text(
+                  'Date',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                    border: const UnderlineInputBorder(),
+                    suffix: IconButton(
+                      onPressed: () {
+                        _restorableDatePickerRouteFuture.present();
+                      },
+                      icon: const Icon(Icons.calendar_month),
+                      color: Colors.white,
+                    ),
+                  )),
+                ),
+                Container(
+                    alignment: Alignment.centerLeft,
+                    child: Checkbox(
+                      value: false,
+                      onChanged: (value) {},
+                      checkColor: Colors.white,
+                    )),
               ]),
         ),
       ),
